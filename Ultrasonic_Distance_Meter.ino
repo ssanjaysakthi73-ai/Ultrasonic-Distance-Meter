ebb58ada09f429c95d3fa40d75dbc0485790dad4
ssanjaysakthi73-ai <ssanjaysakthi73@gmail.com>
2026-06-26 11:16:06 +0530

    Create Ultrasonic_Distance_Meter.ino

---
+++ Ultrasonic_Distance_Meter.ino
@@ -0,0 +1,33 @@
+const int trigPin = 9;
+const int echoPin = 10;
+
+long duration;
+float distance;
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+
+void loop() {
+
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+
+  distance = duration * 0.0343 / 2;
+
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+
+  delay(500);
+}
